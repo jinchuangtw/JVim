056bb75451c9530e9d056af5e93ccd02ef5a7b34
jinchuangtw <jinchuang1999@gmail.com>
2024-06-03 14:57:29 +0800

    Add clangd, markdown, JSON, python setup.

--- init.lua
+++ init.lua
@@ -1,1 +1,45 @@
-require("insis").setup({})
+require("insis").setup({
+  colorscheme = "dracula",
+
+  clangd = {
+    enable = true,
+    lsp = "clangd",
+    linter = "clangd-tidy",
+    formatter = "clang-format",
+    format_on_save = true,
+  },
+
+  markdown = {
+    enable = true,
+    mkdnflow = {
+      next_link = "gn",
+      prev_link = "gp",
+      next_heading = "gj",
+      prev_heading = "gk",
+      follow_link = "gd",
+      go_back = "<C-o>",
+      toggle_item = "tt",
+    },
+    formatter = "prettier",
+    format_on_save = true,
+    wrap = true,
+    theme = "dark",
+  },
+
+  json = {
+    enable = true,
+    lsp = "jsonls",
+    ---@type "jsonls" | "prettier"
+    formatter = "jsonls",
+    format_on_save = true,
+  },
+
+  python = {
+    enable = true,
+    lsp = "pylsp", -- can be pylsp or pyright
+    -- pip install black
+    -- asdf reshim python
+    formatter = "black",
+    format_on_save = true,
+  },
+})
