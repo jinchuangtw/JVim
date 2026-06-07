b54c79aaace655dc266f377febe958b0643ca78d
jinchuangtw <jinchuang1999@gmail.com>
2026-06-07 15:15:59 +0800

    Upgrade clangd version to clangd-18

--- lua/jvim/lsp/config/clangd.lua
+++ lua/jvim/lsp/config/clangd.lua
@@ -1,38 +1,50 @@
 local common = require("jvim.lsp.common-config")
 local util = require("lspconfig.util")
 
-common.capabilities.offsetEncoding = "utf-8"
-
 local home = vim.fn.expand("~")
 
+-- copy common capabilities and add offsetEncoding for clangd
+local capabilities = vim.tbl_deep_extend("force", {}, common.capabilities, {
+  offsetEncoding = { "utf-16" },
+})
+
 local opts = {
-  capabilities = common.capabilities,
+  capabilities = capabilities,
   flags = common.flags,
 
-  -- For PlatformIO projects, clangd must start from the directory that owns
-  -- compile_commands.json / platformio.ini, not just the nearest .git root.
+  -- C/C++ root dir detection logic:
+  -- 1. Has compile_commands.json
+  -- 2. PlatformIO projects: platformio.ini
+  -- 3. CMake projects: CMakeLists.txt
+  -- 4. ROS/catkin package：package.xml
+  -- 5. .git
   root_dir = function(fname)
-    return util.root_pattern("compile_commands.json", "platformio.ini")(fname)
-      or util.root_pattern(".git")(fname)
-      or util.path.dirname(fname)
+    return util.root_pattern("compile_commands.json", "platformio.ini", "CMakeLists.txt", "package.xml", ".clangd")(
+      fname
+    ) or util.root_pattern(".git")(fname) or util.path.dirname(fname)
   end,
 
   cmd = {
-    "clangd",
+    -- use "clangd" instead of "clangd-18" because the latter is not available in all distros yet.
+    -- change it to clangd-18 if you want to use clangd 18 features and your system has it.
+    "clangd-18",
+
     "--background-index",
     "--pch-storage=memory",
     "--clang-tidy",
     "--completion-style=detailed",
 
-    -- Let clangd query PlatformIO ESP toolchains for builtin/system headers.
-    -- This is required for headers such as machine/endian.h, freertos/*, hal/*.
+    -- let clangd query PlatformIO cross compiler，
     "--query-driver="
       .. home
       .. "/.platformio/packages/toolchain-xtensa-esp32s3/bin/xtensa-esp32s3-elf-*,"
       .. home
       .. "/.platformio/packages/toolchain-xtensa-esp32/bin/xtensa-esp32-elf-*,"
       .. home
-      .. "/.platformio/packages/toolchain-riscv32-esp/bin/riscv32-esp-elf-*",
+      .. "/.platformio/packages/toolchain-riscv32-esp/bin/riscv32-esp-elf-*,"
+      .. "**/xtensa-esp32s3-elf-*,"
+      .. "**/xtensa-esp32-elf-*,"
+      .. "**/riscv32-esp-elf-*",
   },
 
   init_options = {
